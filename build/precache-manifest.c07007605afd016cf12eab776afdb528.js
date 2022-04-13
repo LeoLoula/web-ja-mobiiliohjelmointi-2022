deaa7700fea3950f93c8ce9aba5191478ed72a08
Leo Loula <77982568+LeoLoula@users.noreply.github.com>
2022-04-13 17:37:02 +0300

    commit

---
+++ build/precache-manifest.c07007605afd016cf12eab776afdb528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d064e7de643bf73c1d5",
+    "url": "/static/css/main.fd8668b6.chunk.css"
+  },
+  {
+    "revision": "4d064e7de643bf73c1d5",
+    "url": "/static/js/main.5c054cb9.chunk.js"
+  },
+  {
+    "revision": "7d85caf2a602f0a14c63",
+    "url": "/static/js/runtime~main.ae86a450.js"
+  },
+  {
+    "revision": "ed32d8e8c6a14b488963",
+    "url": "/static/js/2.d5ace581.chunk.js"
+  },
+  {
+    "revision": "dfef2813c51b7c1e772e",
+    "url": "/static/js/3.c015642e.chunk.js"
+  },
+  {
+    "revision": "293ea768110f7c2da73f57275c6449cd",
+    "url": "/index.html"
+  }
+];
